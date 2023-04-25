922353cd37a00e11a6e72c076d518602a2f65830
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-04-24 19:50:05 -0700

    add annotation

--- promptop/prototype.py
+++ promptop/prototype.py
@@ -1,18 +1,20 @@
 import os
 from io import BytesIO
+from typing import Optional
 
 import openai
 import requests
 from PIL import Image
 
 ## Params
-_TEMPERATURE = 0.3
+_TEMPERATURE: float = 0.3
+_OUTPUT_DIR: str = "out/"
 
 # OpenAI API: https://platform.openai.com/docs/api-reference/completions/create
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def get_chatgpt_response(prompt, expected_tokens=200):
+def get_chatgpt_response(prompt: str, expected_tokens: int = 200) -> Optional[str]:
     try:
         response = openai.ChatCompletion.create(
             # gpt-4 api is not available for developers yet.
@@ -25,11 +27,13 @@ def get_chatgpt_response(prompt, expected_tokens=200):
         return response.choices[0].message.content.strip()
     except openai.error.RateLimitError:
         print("> Caught RateLimitError but we don't have retry feature yet.")
+        return None
     except Exception as err:
         print(f"> Unexpected {err=}, {type(err)=}")
+        raise
 
 
-def generate_dalle_image(prompt):
+def generate_dalle_image(prompt: str) -> str:
     response = openai.Image.create(
         prompt=prompt,
         n=1,
@@ -38,13 +42,14 @@ def generate_dalle_image(prompt):
     return response.data[0].url
 
 
-def save_image(image_url, file_name):
+def save_image(image_url: str, file_name: str) -> None:
     response = requests.get(image_url)
     img = Image.open(BytesIO(response.content))
-    img.save(file_name)
+    img.save(_OUTPUT_DIR + file_name)
+    return None
 
 
-def main():
+def main() -> None:
     print(">>>type stop to end, type go to gen image<<<\n")
     basic_role = (
         "You're a Prompt Engineer. You help people enrich their description into "
@@ -58,16 +63,23 @@ def main():
     user_prompt = input(
         "> Please provide a brief description of the image you want to generate: "
     )
+    refined_prompt: Optional[str] = user_prompt
     while user_prompt != "stop" and user_prompt != "go":
-        refined_prompt = get_chatgpt_response(user_prompt)
+        if (user_prompt is not None):
+            refined_prompt = get_chatgpt_response(user_prompt)
+        if (refined_prompt is None):
+            return
         print(f"AI: {refined_prompt}")
         user_prompt = input("> ")
 
     if user_prompt == "stop":
         print("> Bye!")
-    image_url = generate_dalle_image(refined_prompt)
-    save_image(image_url, "generated_image.png")
-    print("> Image saved as 'generated_image.png'")
+    if (refined_prompt is not None):
+        image_url = generate_dalle_image(refined_prompt)
+        save_image(image_url, "generated_image.png")
+        print("> Image saved as 'generated_image.png'")
+    else:
+        print("> Skipped. Emptry refined_prompt")
 
     # while True:
     #     feedback = input(
