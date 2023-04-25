8b20e8f552ff3f142a49cf37c8f57694c0870459
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-04-24 17:52:10 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from engine._version import __version__
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='prompt_optimizer',
+    python_requires=">3.11",
+    version=__version__,
+    url='https://github.com/skcoirz/Prompt-Optimizer',
+    description="Use GPT to optimize Stable Diffusion prompts",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Skcoirz',
+    install_requires=[
+        "openai",
+    ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+    ],
+)
+
