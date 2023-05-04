cc181e92ef4642df8e747eca0d90695518b30e53
Mike Wang <62768671+skcoirz@users.noreply.github.com>
2023-05-04 13:05:24 -0700

    added the first unittest

---
+++ tests/unit_tests/test_root_components.py
@@ -0,0 +1,10 @@
+import unittest
+from promptop.__init__ import __all__
+
+class TestRootComponent(unittest.TestCase):
+	def test_root_components(self):
+		assert __all__ == sorted([
+			"OpenAIApiHandler",
+		    "OpenAIModel",
+		    "PipeContainer",
+		    "RepoID",]), "root public component is changed. please make sure it's intended."
