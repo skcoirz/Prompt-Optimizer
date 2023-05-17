12b87d660c52b5e2f150472da942ab4cbf59d675
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-16 22:28:05 -0700

    http_server example

--- frontend/test.ts
+++ frontend/test.ts
@@ -16,4 +16,33 @@ async function print_file_names() {
     }
 }
 
-await print_file_names();
+// await print_file_names();
+
+import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+async function serve_http() {
+
+    // deno-lint-ignore no-unused-vars
+    const handler = async (request: Request): Promise<Response> => {
+    const resp = await fetch("https://api.github.com/users/denoland", {
+        // The init object here has an headers object containing a
+        // header that indicates what type of response we accept.
+        // We're not specifying the method field since by default
+        // fetch makes a GET request.
+        headers: {
+        accept: "application/json",
+        },
+    });
+
+    return new Response(resp.body, {
+        status: resp.status,
+        headers: {
+        "content-type": "application/json",
+        },
+    });
+    };
+
+    console.log("Listening on http://localhost:8000");
+    serve(handler);
+}
+
+serve_http();
