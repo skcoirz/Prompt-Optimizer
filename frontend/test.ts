2dd49a2221bd567f7ca2fe8716e615604560bad2
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-16 22:20:16 -0700

    example of file content reading

--- frontend/test.ts
+++ frontend/test.ts
@@ -1,7 +1,19 @@
-const url = Deno.args[0];
-const res = await fetch(url);
+async function fetch_web() {
+    const url = Deno.args[0];
+    const res = await fetch(url);
 
-const body = new Uint8Array(await res.arrayBuffer());
-await Deno.stdout.write(body);
+    const body = new Uint8Array(await res.arrayBuffer());
+    await Deno.stdout.write(body);
+}
 
-export { };
+// await fetch_web();
+
+async function print_file_names() {
+    const filenames = Deno.args;
+    for (const filename of filenames) {
+      const file = await Deno.open(filename);
+      await file.readable.pipeTo(Deno.stdout.writable);
+    }
+}
+
+await print_file_names();
