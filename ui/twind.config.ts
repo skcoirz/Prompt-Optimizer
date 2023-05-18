1d999fc2c82bccb71e928c600407fa719cd72c6d
Mike W <62768671+skcoirz@users.noreply.github.com>
2023-05-17 21:49:14 -0700

    updated configs to support twind v1 colors

--- ui/twind.config.ts
+++ ui/twind.config.ts
@@ -1,5 +1,12 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "twind_fresh_plugin/twind.ts";
+import { defineConfig } from "twind";
+// twind preset
+import presetAutoPrefix from "twind-preset-autoprefix";
+import presetTailWind from "twind-preset-tailwind";
 
 export default {
+  ...defineConfig({
+    presets: [presetAutoPrefix(), presetTailWind()],
+  }),
   selfURL: import.meta.url,
 } as Options;
